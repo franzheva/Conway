8028f7032fe1bb694e435d4d723159902b30cc7b
Olena Franzheva GG <olena.franzheva@geekgroupllc.com>
2020-08-13 17:35:56 +0300

    try for merge

--- Conway/Function.cs
+++ Conway/Function.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,86 +13,228 @@ namespace Conway
 {
     public partial class Function : Form
     {
-      CheckBox[] cb = new CheckBox[18];
+        
+        public delegate decimal AllCellsFunc(decimal x, decimal y);        
+        public CaclulationFunctionVM funcParsing = new CaclulationFunctionVM();
+        public AllCellsFunc allCellf;
+        private readonly Form1 mainForm;
         TextBox[] tb = new TextBox[9];
-        List<int> Parameters=new List<int>();
-        decimal[] innerParameters = new decimal[9];
-        string mainFunction = string.Empty;
-        public int fieldSize = 0;
+       
+        public decimal[] innerParameters = new decimal[9];
+        public bool currentSeparate = false;
+        public int HeightImg = 0;
+        public int WidthImg = 0;
         public int scale = 0;
-        public Function()
+
+        bool initFromImage = false;
+        public Function(Form1 form)
         {
             InitializeComponent();
-           
+            
+            ClearControlls();
+            mainForm = form;
         }
 
         private void Function_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'cellularAutomataDataSet.Common_AnalyticalCA' table. You can move, or remove it, as needed.
+            //this.common_AnalyticalCATableAdapter.Fill(this.cellularAutomataDataSet.Common_AnalyticalCA);
             int x, y;
             for (int i = 0; i < 9; i++)
             {
                 x = (i == 0 || i == 7 || i == 6) ? 0 : (i == 1 || i == 8 || i == 5) ? 30 : 60;
                 y = (i == 7 || i == 8 || i == 3) ? 30 : (i == 0 || i == 1 || i == 2) ? 0 : 60;
                 panel2.Controls.Add(tb[i] = new TextBox() { Location = new Point(x, y), Width=28 });
-            }
+            }            
+            ok.Enabled = false;
         }
 
+        //Set function button
         private void button1_Click(object sender, EventArgs e)
         {
-           
-            for (int p=0;p<9;p++)
+            if (fieldsizeHeighttb.Text != "" && fieldsizeHeighttb.Text != "Height" && fieldsizeWidthtb.Text != "Width" && fieldsizeWidthtb.Text != "" && scaletb.Text != "")
             {
-                if (tb[p].Text != "")
-                    innerParameters[p] = Convert.ToDecimal(tb[p].Text);
-                else
-                    innerParameters[p] = 0;
-                textBox1.Text += innerParameters[p].ToString() + " ";
-            }
-            fieldSize = Convert.ToInt32(fieldsizetb.Text);
-            scale = Convert.ToInt32(scaletb.Text);
-            mainFunction = tentCB.Checked ? "Tent" : "Logistic";
-         }
+                try
+                {
+                    var uiValidation = 0.0m;
+                    for (int p = 0; p < 9; p++)
+                    {
+                        if (tb[p].Text != "")
 
+                            innerParameters[p] = Convert.ToDecimal(tb[p].Text);
+
+                        else
+                            innerParameters[p] = p == 8 ? 1 : 0;
+                        uiValidation += innerParameters[p];
+                    }
+                    if (uiValidation == 1)
+                        tb[8].Text = "1";
+                    HeightImg = Convert.ToInt32(fieldsizeHeighttb.Text);
+                    WidthImg = Convert.ToInt32(fieldsizeWidthtb.Text);
+                    scale = Convert.ToInt32(scaletb.Text);
+                    allCellf = new AllCellsFunc(funcParsing.FunctionForAllParsed("return 4*(1-0.5m*y)*x*(1-x);"));//CalcFunctionCB.SelectedValue.ToString()
+                    ok.Enabled = true;
+                    weightsLbl.ForeColor = Color.Green;
+                    weightsLbl.Text = "Data set is completed. Press OK to continue";
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message == "Input string was not in a correct format.")
+                        weightsLbl.Text = "*Values for cell weights, field size and scale should be decimals!";
+                    else
+                        weightsLbl.Text = "*There is some problem with chosen function";
+                }
+            }
+            else
+            {
+                Validation();
+            }           
+        }
+        public void Validation()
+        {
+
+            fieldSizeEmpty.Text = (fieldsizeHeighttb.Text != "" && fieldsizeHeighttb.Text != "Height")
+                && (fieldsizeWidthtb.Text != "Width" && fieldsizeWidthtb.Text != "") ? "" : "*Field size is required";
+            ScaleEmpty.Text = scaletb.Text != "" ? "" : "*Scale is required";
+        }
         private void Clear_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 18; i++)
+            ClearControlls();
+        }
+        public void ClearControlls()
+        {
+            fieldsizeHeighttb.ReadOnly = false;
+            fieldsizeHeighttb.Text = "Height";
+            fieldsizeHeighttb.ForeColor = Color.DarkGray;
+            HeightImg = 0;
+
+            fieldsizeWidthtb.ReadOnly = false;
+            fieldsizeWidthtb.Text = "Width";
+            fieldsizeWidthtb.ForeColor = Color.DarkGray;
+            WidthImg = 0;
+
+            scaletb.Text = "";
+            scale = 0;
+
+            fieldSizeEmpty.Text = "";
+            ScaleEmpty.Text = "";
+            weightsLbl.Text = "";
+
+            for (int i = 0; i < 9; i++)
             {
-                textBox1.Text = "";
+                if (tb[i] != null)
+                    tb[i].Text = "";
             }
 
+            ok.Enabled = false;
+
+            ResetCBData();
         }
-        public List<int> GetFunc()
-        {            
-            return Parameters;
-        }
-        public decimal[] innerFunc()
-        {
-            return innerParameters;
-        }
-        public string MainFunction()
-        {
-            return mainFunction;
-        }
+       
         private void ok_Click(object sender, EventArgs e)
         {
+            if (initFromImage)
+            {
+                MessageBox.Show("Initial Data uploaded from image will be applied");
+            }
+            else
+            {
+                MessageBox.Show("Random Initial Data will be applied");
+                mainForm.SetInitial();
+            }
+           
+            mainForm.isFirstLaunch = true;
             this.Hide();
+
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
-
-        private void tentCB_CheckedChanged(object sender, EventArgs e)
-        {
-            if (logisticCB.Checked)
-                logisticCB.Checked = false;
+        public void ResetCBData()
+        {            
+            this.commonAnalyticalCABindingSource.ResetBindings(true);
+            CalcFunctionCB.Refresh();
         }
 
-        private void logisticCB_CheckedChanged(object sender, EventArgs e)
+        private void AddNewFunctionBtn_Click(object sender, EventArgs e)
         {
-            if (tentCB.Checked)
-                tentCB.Checked = false;
+            AddNewFunction addFuncForm = new AddNewFunction(this);           
+            addFuncForm.Show();
         }
-    }
+
+        private void UploadInit_btn_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult != DialogResult.OK) return;
+            ImageData(Upload(openFileDialog.FileName));            
+        }
+        private string Upload(string fileName)
+        {           
+            try
+            {
+                var data = File.ReadAllBytes(fileName);
+                using (FileStream fcreate = File.Open(fileName, FileMode.Create))
+                {
+                    fcreate.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return fileName;
+        }
+        public void ImageData(string path)
+        {
+            if (path != "")
+            {
+                var image = new Bitmap(path);
+                HeightImg = image.Height;
+                WidthImg = image.Width;
+                var initData = new decimal[HeightImg, WidthImg];
+                fieldsizeHeighttb.Text = HeightImg.ToString();
+                fieldsizeHeighttb.ReadOnly = true;
+                fieldsizeWidthtb.Text = WidthImg.ToString();
+                fieldsizeWidthtb.ReadOnly = true;
+                scale = 1;
+                scaletb.Text = scale.ToString();
+                for (int i = 0; i < HeightImg; i++)
+                    for (int j = 0; j < WidthImg; j++)
+                        initData[i, j] = 1 - ((decimal)image.GetPixel(j, i).R) / 255;
+
+                mainForm.SetInitialFromImage(initData);
+                initFromImage = true;
+            }
+        }
+
+        private void FieldsizeHeighttb_MouseDown(object sender, MouseEventArgs e)
+        {
+            fieldsizeHeighttb.Text = "";
+            fieldsizeHeighttb.ForeColor = Color.Black;
+        }
+
+        private void FieldsizeWidthtb_MouseDown(object sender, MouseEventArgs e)
+        {
+            fieldsizeWidthtb.Text = "";
+            fieldsizeWidthtb.ForeColor = Color.Black;
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (useCurrentAsSeparate.Checked)
+            {
+                tb[8].Text = "1";
+                tb[8].ReadOnly = true;
+                currentSeparate = true;
+            }
+            else {
+                tb[8].Text = "";
+                tb[8].ReadOnly = false;
+                currentSeparate = false;
+            }
+        }
+    }    
 }
